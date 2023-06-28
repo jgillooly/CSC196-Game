4e4767bab9e441f4ca242d2b471d388698531667
Joshua <jgillooly@student.neumont.edu>
2023-06-28 14:46:51 -0600

    Day 3

--- source/Engine/Core/Memory.h
+++ source/Engine/Core/Memory.h
@@ -9,8 +9,8 @@ namespace antares {
 
 	class MemoryTracker {
 	public:
-		//void Add(void* address, size_t size);
-		//void Remove(void* address);
+		/*void add(void* address, size_t size);
+		void remove(void* address);*/
 	private:
 
 	};
